4dca590fc5c9e9fb2927766ec238fc8143242a6a
enderxdxd <148904673+enderxdxd@users.noreply.github.com>
2023-12-12 17:11:24 -0300

    Add files via upload

---
+++ RAIZ QUADRADA.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h> //função matématica
+
+int main(){
+	
+	int n1,n2,n3;
+	float res;
+	printf("Digite o numero 1:\n");
+	scanf("%d", &n1);
+	
+	printf("Digite o segundo numero: \n");
+	scanf("%d", &n2);
+	
+	printf("Digite o terceiro numero: \n");
+	scanf("%d", &n3);
+	
+	res=  sqrt(n1+n2+n3);
+	
+	printf ("Resultado : %f", res);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+return 0;	
+	
+	
+	
+	
+}
---
+++ TABUADA.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+	
+	int i,n;
+	
+	for (i=2; i<=20; i+=2 ){
+		
+		printf("%d\n", i);
+	}
+	
+	
+	for (n=3; n<=30; n+=3){
+	printf("\n%d", n);
+	
+	
+}
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+
